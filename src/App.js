7740b9601397f5c79179513c4a36d52e0a9c3185
Vijay Wilson <110322906+Vijay-wilson@users.noreply.github.com>
2023-04-22 19:11:09 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Register from './components/Register';
+// import UserLogin from './components/UserLogin';
+import Userdashboard from './components/Userdashboard';
+import LoginUser from './components/LoginUser';
+import AdminLogin from './components/AdminLogin';
+import Admindashboard from './components/Admindashboard';
+import EmployeeForm from './components/EmployeeForm';
+import TDashboard from './components/TDashboard';
+import Editform from './components/Editform';
+import Home from './components/Home';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <BrowserRouter>
+     <Routes>
+      <Route path="/register" element={<Register/>}></Route>
+      {/* <Route path="/user-login" element={<UserLogin/>}></Route> */}
+      <Route path="/login-user" element={<LoginUser/>}></Route>
+      <Route path="/user-dashboard" element={<Userdashboard/>}></Route>
+      <Route path="/admin-login" element={<AdminLogin/>}></Route>
+      <Route path="/admin-dashboard" element={<Admindashboard/>}></Route>
+      <Route path="/empdataEntry" element={<EmployeeForm/>}></Route>
+      <Route path="/tdashboard" element={<TDashboard/>}></Route>
+      <Route path="/updateEmployee/:id" element={<Editform/>}></Route>
+      <Route path="/" element={<Home/>}></Route>
+      
+
+
+     </Routes>
+     
+     </BrowserRouter>
     </div>
   );
 }
